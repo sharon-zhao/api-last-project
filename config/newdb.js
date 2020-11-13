d4f2c630ff37b1f463228a9a3498bfd5623e7f8e
sharon zhao <xinrui.zhao@yahoo.com>
2020-11-12 21:40:31 -0500

    revert uri

---
+++ config/newdb.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+const fs = require('fs');
+
+const credentials = fs.readFileSync('<path_to_certificate>');
+
+const client = new MongoClient('mongodb+srv://sharon:Zxr2303811992-@cluster0.tw9s5.mongodb.net/testDB?retryWrites=true&w=majority', {
+  sslKey: credentials,
+  sslCert: credentials
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("testDB");
+    const collection = database.collection("testCol");
+    const docCount = await collection.countDocuments({});
+    console.log(docCount);
+    // perform actions using client
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
